992a44442b84402a26378d050d8ded07228f5750
Abhishek kale(overrkill) <zabhishekkale@gmail.com>
2021-04-25 19:59:22 +0530

    basic math

---
+++ 50a_domino/50a_domino.cpp
@@ -0,0 +1,36 @@
+/**
+ * File              : Template.cpp
+ * Author            : Abhishek Kale  <https://github.com/overrkill>
+ * Date              : 24.04.2021
+ * Last Modified Date: 24.04.2021
+ * Last Modified By  : Abhishek Kale  <https://github.com/overrkill>
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define deb(x) cout<<#x<<"="<<x<<endl;
+
+int solve(){
+	// solve the problem
+	int m , n;
+	cin >> m >> n;
+	
+	return (m*n)/2;
+
+}
+
+int main(){
+
+  	// #ifndef JUDGE
+  	// freopen("input.txt","r",stdin);
+  	// freopen("output.txt","w",stdout);
+  	// #endif
+	
+	// int tc;
+	// cin >> tc;
+	//
+	// while(tc--){
+	// }
+  
+	cout << solve() << endl;		
+  }
