58bb48c2e30f418b49bc2259dbcbc0e3c6f08a7c
Abhishek kale(overrkill) <zabhishekkale@gmail.com>
2021-04-25 19:59:02 +0530

    optim approach added

--- 158A-nextround/158A-nextround.cpp
+++ 158A-nextround/158A-nextround.cpp
@@ -2,7 +2,7 @@
  * File              : 158A-nextround.cpp
  * Author            : Abhishek Kale  <https://github.com/overrkill>
  * Date              : 24.04.2021
- * Last Modified Date: 24.04.2021
+ * Last Modified Date: 25.04.2021
  * Last Modified By  : Abhishek Kale  <https://github.com/overrkill>
  */
 
@@ -17,9 +17,16 @@ void solve(){
 	cin >> n >> k;
 	k--;
 	int arr[n];
-	int count = 0;
+	int count = 0 ;
+
+	// naive solution
 	for(int i=0 ;i < n;i++){ cin >> arr[i];}
 	for(int j =0 ;j < n ;j++) if (arr[j]>=arr[k] and arr[j] > 0 ) count++; 
+	
+	// better approach
+	// int count = k ;
+	// for(int it = k ; it < n ; it++){ if ( arr[it] == arr[k] ){ count++; } else { break; } }
+		
 	cout << count;
 }
 
